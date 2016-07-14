9b839470973ba9ebd1db721419a40d7c81dc352a
axu1122 <axu1122@gmail.com>
2016-07-13 23:25:53 -0400

    before adding vedic rishi

--- app/controllers/concerns/horoscopes_controller.rb
+++ app/controllers/concerns/horoscopes_controller.rb
@@ -1,5 +1,5 @@
 class HoroscopesController < ApplicationController
-	before_action :all_signs, only: [:initialize, :show]
+	before_action :all_signs, only: [:initialize, :show, :tomorrow, :yesterday]
 	require 'httparty'
 
 	def index
@@ -8,6 +8,24 @@ class HoroscopesController < ApplicationController
 
 	def show
 		@response = HTTParty.get('https://theastrologer-api.herokuapp.com/api/horoscope/' + @horoscope.name + '/today')
+		@http_party_json = JSON.parse(@response.body)
+		@parsed_horoscope = @http_party_json["horoscope"]
+		@parsed_date = @http_party_json["date"]
+		@parsed_intensity = @http_party_json["meta"]["intensity"]
+		@parsed_mood = @http_party_json["meta"]["mood"]
+	end
+
+	def tomorrow
+		@response = HTTParty.get('https://theastrologer-api.herokuapp.com/api/horoscope/' + @horoscope.name + '/tomorrow')
+		@http_party_json = JSON.parse(@response.body)
+		@parsed_horoscope = @http_party_json["horoscope"]
+		@parsed_date = @http_party_json["date"]
+		@parsed_intensity = @http_party_json["meta"]["intensity"]
+		@parsed_mood = @http_party_json["meta"]["mood"]
+	end
+
+	def yesterday
+		@response = HTTParty.get('https://theastrologer-api.herokuapp.com/api/horoscope/' + @horoscope.name + '/yesterday')
 		@http_party_json = JSON.parse(@response.body)
 		@parsed_horoscope = @http_party_json["horoscope"]
 		@parsed_date = @http_party_json["date"]
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,27 @@
 Rails.application.routes.draw do
-  resources :horoscopes
+  root to: 'horoscopes#index'
+  resources :horoscopes do 
+    member do 
+      get 'tomorrow'
+    end
+
+    member do 
+      get 'yesterday'
+    end
+  end
 end
 
 
 # get '/' => 'tests#test'
 
-#         Prefix Verb   URI Pattern                    Controller#Action
-#     horoscopes GET    /horoscopes(.:format)          horoscopes#index
-#                POST   /horoscopes(.:format)          horoscopes#create
-#  new_horoscope GET    /horoscopes/new(.:format)      horoscopes#new
-# edit_horoscope GET    /horoscopes/:id/edit(.:format) horoscopes#edit
-#      horoscope GET    /horoscopes/:id(.:format)      horoscopes#show
-#                PATCH  /horoscopes/:id(.:format)      horoscopes#update
-#                PUT    /horoscopes/:id(.:format)      horoscopes#update
-#                DELETE /horoscopes/:id(.:format)      horoscopes#destroy
+#              Prefix Verb   URI Pattern                         Controller#Action
+#  tomorrow_horoscope GET    /horoscopes/:id/tomorrow(.:format)  horoscopes#tomorrow
+# yesterday_horoscope GET    /horoscopes/:id/yesterday(.:format) horoscopes#yesterday
+#          horoscopes GET    /horoscopes(.:format)               horoscopes#index
+#                     POST   /horoscopes(.:format)               horoscopes#create
+#       new_horoscope GET    /horoscopes/new(.:format)           horoscopes#new
+#      edit_horoscope GET    /horoscopes/:id/edit(.:format)      horoscopes#edit
+#           horoscope GET    /horoscopes/:id(.:format)           horoscopes#show
+#                     PATCH  /horoscopes/:id(.:format)           horoscopes#update
+#                     PUT    /horoscopes/:id(.:format)           horoscopes#update
+#                     DELETE /horoscopes/:id(.:format)           horoscopes#destroy
