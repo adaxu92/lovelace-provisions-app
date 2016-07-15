5cdc28ee5d8ce6eb94255d081b66f8eb0e076bcb
axu1122 <axu1122@gmail.com>
2016-07-15 11:03:07 -0400

    deployment to heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,40 +1,40 @@
-# Horoscope.delete_all
-# Horoscope.create([
-# 	{
-# 		name: 'aries',
-# 		img: 'http://i.imgur.com/oBB6kHP.png'
-# 	}, {
-# 		name: 'taurus',
-# 		img: 'http://i.imgur.com/eJ0cANL.png'
-# 	}, {
-# 		name: 'gemini',
-# 		img: 'http://i.imgur.com/mc8XRsb.png'
-# 	}, {
-# 		name: 'cancer',
-# 		img: 'http://i.imgur.com/PsWFtDX.png'
-# 	}, {
-# 		name: 'leo',
-# 		img: 'http://i.imgur.com/qZbWPLH.png'
-# 	}, {
-# 		name: 'virgo',
-# 		img: 'http://i.imgur.com/TFVeKK8.png'
-# 	}, {
-# 		name: 'libra',
-# 		img: 'http://i.imgur.com/KJ83Zsk.png'
-# 	}, {
-# 		name: 'scorpio',
-# 		img: 'http://i.imgur.com/SKj3d4O.png'
-# 	}, {
-# 		name: 'sagittarius',
-# 		img: 'http://i.imgur.com/YcNUrLF.png'
-# 	}, {
-# 		name: 'capricorn',
-# 		img: 'http://i.imgur.com/4S3EWwz.png'
-# 	}, {
-# 		name: 'aquarius',
-# 		img: 'http://i.imgur.com/nBwcvPe.png'
-# 	}, {
-# 		name: 'pisces',
-# 		img: 'http://i.imgur.com/QIzB7p9.png'
-# 	}
-# ])
+Horoscope.delete_all
+Horoscope.create([
+	{
+		name: 'aries',
+		img: 'http://i.imgur.com/oBB6kHP.png'
+	}, {
+		name: 'taurus',
+		img: 'http://i.imgur.com/eJ0cANL.png'
+	}, {
+		name: 'gemini',
+		img: 'http://i.imgur.com/mc8XRsb.png'
+	}, {
+		name: 'cancer',
+		img: 'http://i.imgur.com/PsWFtDX.png'
+	}, {
+		name: 'leo',
+		img: 'http://i.imgur.com/qZbWPLH.png'
+	}, {
+		name: 'virgo',
+		img: 'http://i.imgur.com/TFVeKK8.png'
+	}, {
+		name: 'libra',
+		img: 'http://i.imgur.com/KJ83Zsk.png'
+	}, {
+		name: 'scorpio',
+		img: 'http://i.imgur.com/SKj3d4O.png'
+	}, {
+		name: 'sagittarius',
+		img: 'http://i.imgur.com/YcNUrLF.png'
+	}, {
+		name: 'capricorn',
+		img: 'http://i.imgur.com/4S3EWwz.png'
+	}, {
+		name: 'aquarius',
+		img: 'http://i.imgur.com/nBwcvPe.png'
+	}, {
+		name: 'pisces',
+		img: 'http://i.imgur.com/QIzB7p9.png'
+	}
+])
